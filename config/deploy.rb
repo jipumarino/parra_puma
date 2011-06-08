0d64ef3be279467557898509a331c9cd44b0c456
Juan Ignacio Pumarino <jipumarino@gmail.com>
2011-06-08 09:04:24 -0400

    deploy auto

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,7 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+set :rvm_ruby_string, '1.9.2-p0@rails-3.1'
+
 set :application, "parra_puma"
 set :repository,  "git://github.com/jipumarino/parra_puma.git"
 set :deploy_to, "/var/apps/parra_puma"
@@ -14,6 +18,7 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "chown nginx:nginx -R #{release_path}"
+    run "cd #{release_path} && passenger stop -p 3001 && passenger start -p 3001 -d -e production"
   end
 end
